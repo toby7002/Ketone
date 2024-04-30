8a2174bc1a13fdd278935ec188d18cd9fbe206d5
Nguyen Minh Tho <minhthoj27@icloud.com>
2024-04-30 17:03:19 +0700

    Optimize code

--- src/main/kotlin/dev/toby7002/ketone/datagen/models/KItemModelProvider.kt
+++ src/main/kotlin/dev/toby7002/ketone/datagen/models/KItemModelProvider.kt
@@ -3,17 +3,23 @@ package dev.toby7002.ketone.datagen.models
 import dev.toby7002.ketone.KItems
 import dev.toby7002.ketone.Ketone
 import net.minecraft.data.PackOutput
+import net.minecraft.world.item.Item
 import net.neoforged.neoforge.client.model.generators.ItemModelProvider
 import net.neoforged.neoforge.common.data.ExistingFileHelper
 
 class KItemModelProvider(output: PackOutput, existingFileHelper: ExistingFileHelper) :
     ItemModelProvider(output, Ketone.MOD_ID, existingFileHelper) {
     override fun registerModels() {
-        basicItem(KItems.GLASS_CUTTER.get())
-        basicItem(KItems.POLISHED_QUARTZ.get())
-        basicItem(KItems.ICE_BREAKER.get())
-        basicItem(KItems.GUIDEBOOK.get())
-        basicItem(KItems.TINY_COAL.get())
-        basicItem(KItems.TINY_CHARCOAL.get())
+        val basicItems =
+            listOf<Item>(
+                KItems.GLASS_CUTTER.get(),
+                KItems.POLISHED_QUARTZ.get(),
+                KItems.ICE_BREAKER.get(),
+                KItems.GUIDEBOOK.get(),
+                KItems.TINY_COAL.get(),
+                KItems.TINY_CHARCOAL.get()
+            )
+
+        basicItems.forEach { item -> basicItem(item) }
     }
 }
